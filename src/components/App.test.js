4b07cc66617a217e50d7e8f198e8c136924e340b
Richard Hounslow <rhounslow@gmail.com>
2021-02-24 18:42:44 +1100

    First basic 'shallow' test of the <App />

---
+++ src/components/App.test.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot();
+});
