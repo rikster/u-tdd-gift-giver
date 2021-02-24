dd87e27ba484daf558d1a92cb510dbd39a069cdc
Richard Hounslow <rhounslow@gmail.com>
2021-02-24 19:13:12 +1100

    TDD init state and adding `gifts` to state

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
@@ -6,4 +6,13 @@ const app = shallow(<App />);
 
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('inits the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([]);
+});
+
+it('add a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
