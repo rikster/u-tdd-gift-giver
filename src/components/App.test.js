f73fec91044b9191db454b2da901ef6e7df8dd7e
Richard Hounslow <rhounslow@gmail.com>
2021-02-25 14:31:47 +1100

    move App to BDD

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,23 +2,35 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+// describe block setup BDD, update snapshot required via pressing 'u'
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('inits the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('inits the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('add a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  describe('when clicking the `add-gift` button', () => {
+    // before each of the it blocks
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
 
-// have to equal 2 to prevent test polution
-it('should add a new gift to the rendered list when clicking the `add gift`', function () {
-  app.find('.btn-add').simulate('click');
-  expect(app.find('.gift-list').children().length).toEqual(2);
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('add a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    // have to equal 2 to prevent test pollution
+    it('should add a new gift to the rendered list', function () {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  });
 });
